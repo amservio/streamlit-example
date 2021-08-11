209139f05f00b3370dfe3360a5a0f8a20cdfd04a
Alexandre Servio <alexandremservio@gmail.com>
2021-08-11 04:06:46 -0300

    Playing with multiselect

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.datasets import *
 
-option = st.sidebar.selectbox(
+sns.set_style("darkgrid")
+
+option = st.sidebar.radio(
     'Choose dataset',
     ['boston', 'iris', 'diabetes']
 )
@@ -13,41 +15,46 @@ option = st.sidebar.selectbox(
 if option == 'boston':
     st.title("Boston housing dataset")
 
+    graph_menu = st.sidebar.multiselect('Choose graphs', [
+        "CRIM Histogram",
+        "Charles River pricing effect",
+        "Age x Industrialization",
+        "Pupil/teacher x NOX regression"
+        ])
+
     boston = load_boston()
     df = pd.DataFrame(boston.data, columns=boston.feature_names)
     df['target'] = pd.Series(boston.target)
 
-    CRIM_graph = st.sidebar.checkbox("CRIM Histogram")
-    CHAS_graph = st.sidebar.checkbox("Charles River pricing effect")
-    INDUS_graph = st.sidebar.checkbox("Age x Industrialization")
-    NOX_graph = st.sidebar.checkbox("Pupil/teacher x NOX regression")
-
     st.header("Raw data")
     st.write(df)
 
-    sns.set_style("darkgrid")
-
-    if CRIM_graph:
+    if "CRIM Histogram" in graph_menu:
         st.header("CRIM Histogram")
         g = sns.displot(df, x='CRIM')
         st.pyplot(g)
 
-    if CHAS_graph:
+    if "Charles River pricing effect" in graph_menu:
         st.header("Charles River pricing effect")
         g = sns.catplot(data=df, x='CHAS', y='target', kind = 'swarm')
         st.pyplot(g)
 
-    if INDUS_graph:
+    if "Age x Industrialization" in graph_menu:
         st.header("Age x Industrialization")
         g = sns.relplot(data=df, x='AGE', y='INDUS')
         st.pyplot(g)
 
-    if NOX_graph:
+    if "Pupil/teacher x NOX regression" in graph_menu:
         st.header("Pupil/teacher x NOX regression")
         g = sns.jointplot(data=df, x='NOX', y='INDUS', kind="reg", truncate=False)
         st.pyplot(g)
+
 elif option == 'iris':
     st.title("Iris flower dataset")
+
+    graph_menu = st.sidebar.multiselect('Choose graphs', [
+            "Boxplots"
+        ])
 
     iris = load_iris()
     df = pd.DataFrame(iris.data, columns=iris.feature_names)
@@ -55,8 +62,20 @@ elif option == 'iris':
 
     st.header('Raw data')
     st.write(df)
+
+    if "Boxplots" in graph_menu:
+        st.header("Boxplots")
+        b = st.selectbox("", df.columns)
+
+        g = sns.catplot(data=df, y=b, kind='box')
+        st.pyplot(g)
+
 elif option == 'diabetes':
     st.title("Diabetes dataset")
+
+    graph_menu = st.sidebar.multiselect('Choose graphs', [
+            "Boxplots"
+        ])
 
     diabetes = load_diabetes()
     df = pd.DataFrame(diabetes.data, columns=diabetes.feature_names)
@@ -64,5 +83,13 @@ elif option == 'diabetes':
 
     st.header('Raw data')
     st.write(df)
+
+    if "Boxplots" in graph_menu:
+        st.header("Boxplots")
+        b = st.selectbox("", df.columns)
+
+        g = sns.catplot(data=df, y=b, kind='box')
+        st.pyplot(g)
+
 else:
     pass
